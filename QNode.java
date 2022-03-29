0068f6df0d0b4c1c96a7c0f678d70f21829b3314
Shubham <nhaldes@gmail.com>
2022-03-29 22:08:39 +0530

    Added: Add Elements In Queue

---
+++ QNode.java
@@ -0,0 +1,16 @@
+package www.bridgelabz.Queue;
+
+public class QNode {
+	
+	  int data;
+      QNode next;
+
+      //Parameter and Default Constructors of QNode
+      public QNode(int x) {
+          this.data = x;
+          this.next = null;
+      }
+      public QNode(){
+      }
+
+}
---
+++ Queue.java
@@ -0,0 +1,48 @@
+package www.bridgelabz.Queue;
+
+public class Queue {
+	
+
+    //Represent the 'Q Front and Rear Pointers for the EnQue and DeQue' as of the singly linked list
+    public QNode front = null;
+    public QNode rear = null;
+
+    //Method to push int element in the Queue using front rear
+    public void enQue(int x){
+        QNode node = new QNode(x);
+        if (front == null){
+            System.out.println("Queue is Underflow and Empty");
+            front = node;
+        }else {
+            rear.next = node;
+        }
+        rear = node;
+        System.out.println("Successfully EnQue the data : " + x);
+    }
+
+    /// Method to display Queue in First In First Out Order
+    public void display() {
+        QNode temp = new QNode();
+        temp = front;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Queue Que = new Queue();
+
+        //Method to push int element in the Queue using front rear
+        Que.enQue(56);
+        Que.enQue(30);
+        Que.enQue(70);
+
+        Que.display();
+
+
+    }
+
+
+}
